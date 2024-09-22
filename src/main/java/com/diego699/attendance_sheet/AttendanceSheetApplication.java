608aa7e3542d7e73e02b99a35a7c89c16a9fbe60
Diego Leonardo <diegoleonardo9966@gmail.com>
2024-09-22 10:59:42 -0300

    inicio do projeto

---
+++ src/main/java/com/diego699/attendance_sheet/AttendanceSheetApplication.java
@@ -0,0 +1,13 @@
+package com.diego699.attendance_sheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceSheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceSheetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diego699/attendance_sheet/AttendanceSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego699.attendance_sheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceSheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
