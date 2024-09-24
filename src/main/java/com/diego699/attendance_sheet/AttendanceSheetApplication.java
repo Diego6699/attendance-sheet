75e001d98a71e8b62a34cc56e0a095eb30308312
Diego Leonardo <diegoleonardo9966@gmail.com>
2024-09-23 21:42:59 -0300

    criacao da entidade Evento

--- src/main/java/com/diego699/attendance_sheet/AttendanceSheetApplication.java
+++ src/main/java/com/diego699/attendance_sheet/AttendanceSheetApplication.java
@@ -1,13 +1,22 @@
 package com.diego699.attendance_sheet;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AttendanceSheetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AttendanceSheetApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(String[] args){
+		return runner ->{
+
+		};
 	}
 
 }
--- src/main/java/com/diego699/attendance_sheet/entity/Evento.java
+++ src/main/java/com/diego699/attendance_sheet/entity/Evento.java
@@ -1,2 +1,18 @@
-package com.diego699.attendance_sheet.entity;public class Evento {
+package com.diego699.attendance_sheet.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Evento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "nome_evento")
+    private String nomeEvento;
+    @Column
+    private List<LocalDate> datasEventos;
 }
